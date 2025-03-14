817b7af6372dd22fed0c59daab4e88284becfe28
Tendchtte <mokona004681839@gmail.com>
2025-03-15 00:50:27 +0900

    [add]fablicテンプレート作成

---
+++ src/client/java/tendchtte/magica_glyphstone/MagicaGlyphstoneClient.java
@@ -0,0 +1,10 @@
+package tendchtte.magica_glyphstone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicaGlyphstoneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
